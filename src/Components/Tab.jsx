4e0af6f49bf15a41aa5dd8595a534dd1047a4a61
Jonathan Shine <jonathanhshine@gmail.com>
2021-09-02 16:14:10 +0200

    finished the Tab component using icons from the react-icons library and styled it with its own scss stylesheet

--- src/Components/Tab.jsx
+++ src/Components/Tab.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { FaTv } from 'react-icons/fa';
+import { BsGrid3X3 } from 'react-icons/bs';
+import { MdPersonPin } from 'react-icons/md';
+import { VscBookmark } from 'react-icons/vsc';
 
 const Tab = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="tabContainer">
+      <a href="">
+        <span>
+            <div className="tabIcons"><BsGrid3X3/></div>
+            POSTS
+        </span>
+      </a>
+      <a href="">
+        <span>
+            <div className="tabIcons"><FaTv/></div>
+            IGTV
+        </span>
+      </a>
+      <a href="">
+        <span>
+            <div className="tabIcons"><VscBookmark/></div>
+            SAVED
+        </span>
+      </a>
+      <a href="">
+        <span>
+            <div className="tabIcons"><MdPersonPin/></div>
+            TAGGED
+        </span>
+      </a>
+    </div>
+  );
+};
 
-export default Tab
+export default Tab;
