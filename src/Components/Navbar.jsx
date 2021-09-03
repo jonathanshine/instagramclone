03699246c3df40275aba2a5beb38e63df41131a2
Jonathan Shine <jonathanhshine@gmail.com>
2021-09-03 10:19:38 +0200

    minor stylistic changes, mostly to the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,19 +6,20 @@ import { CgHome } from 'react-icons/cg';
 const Navbar = () => {
     return (
         <nav>
-            <div>
-                <img src={`${process.env.PUBLIC_URL}/img/instaLogo.png`} alt='instagram logo' />
-            </div>
-            <div>
-                <input type="search" name="search" id="search" placeholder='&#8981; 
-Search'/>
-            </div>
-            <div className='icons'>
-                <a href="#"><CgHome/></a>
-                <a href="#"><FaRegPaperPlane/></a>
-                <a href="#"><FaRegCompass/></a>
-                <a href="#"><FaRegHeart/></a>
-                <a href="#"><FaRegCircle/></a>
+            <div className="navInside">
+                <div>
+                    <img src={`${process.env.PUBLIC_URL}/img/instaLogo.png`} alt='instagram logo' />
+                </div>
+                <div>
+                    <input type="search" name="search" id="search" placeholder='&#8981;Search'/>
+                </div>
+                <div className='icons'>
+                    <a href="#"><CgHome/></a>
+                    <a href="#"><FaRegPaperPlane/></a>
+                    <a href="#"><FaRegCompass/></a>
+                    <a href="#"><FaRegHeart/></a>
+                    <a href="#"><FaRegCircle/></a>
+                </div>
             </div>
         </nav>
     )
